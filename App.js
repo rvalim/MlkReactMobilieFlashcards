b12469bf3b681d6167b81093d3a7e4df5abbe589
unknown <ricardo.valim@gmail.com>
2019-04-03 12:16:16 +0100

    fixing import path

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux'
 import { StyleSheet, Text, View } from 'react-native';
-import store from './store/store'
+import store from './src/store'
 
 export default class App extends React.Component {
   render() {
