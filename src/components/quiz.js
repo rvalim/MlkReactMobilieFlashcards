c1ffadda500827650e300220dc5c16c6d70df2a5
unknown <ricardo.valim@gmail.com>
2019-04-04 22:38:15 +0100

    created a stack of cards, showing the last one, and pop up a new one user answer

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,22 +1,88 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Alert, Text, View, Button, TextInput } from 'react-native'
 
-const Quiz = ({ cards }) => {
-    const formatCard = (card) => {
-        console.log(card)
+class Quiz extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            answeredId: [],
+            answers: {},
+            popedCard: null, 
+            quizFinish: false
+        }
+    }
+
+    componentDidMount(){
+        this.pop()
+    }
+
+    handleAnswer(id, isCorrect) {
+        this.setState((prev, props) => {
+            return {
+                answers: {
+                    ...prev.answers,
+                    [id]: isCorrect.toString()
+                }
+                , answeredId: [
+                    ...prev.answeredId,
+                    id
+                ]
+            }
+        }, () => this.pop())
+    }
+
+    formatCard(card) {
+        if (!card) return
         return (
             <View key={card.id}>
                 <Text>{card.question}</Text>
+                <Button
+                    title="Correct"
+                    onPress={() => this.handleAnswer(card.id, true)} />
+                <Button
+                    title="Incorrect"
+                    onPress={() => this.handleAnswer(card.id, false)} />
             </View>
         )
     }
 
-    return <View>
-        {cards ?
-            cards.map(p => formatCard(p)) :
-            <Text>You can't answer quizes without cards</Text>}
-    </View>
+    showScore(){
+        const {answeredId, answers, quizFinish} = this.state
+        if (!quizFinish) return 
+
+        const counter = {}
+        Object.keys(answers).forEach(p => counter[answers[p]] = (counter[answers[p]] || 0) + 1)
+
+        return (
+            <View>
+                <Text>false: {counter['false']}</Text>
+                <Text>true: {counter['true']}</Text>
+            </View>
+        )
+    }
+
+    pop() {
+        const {cards} = this.props
+        const {answeredId} = this.state
+        const popedCard = cards.filter(p => answeredId.indexOf(p.id) === -1).pop()
+        this.setState({popedCard})
+        
+        if (!popedCard)
+            this.setState({quizFinish:true})
+    }
+
+    render() {
+        return (
+            <View>
+                {this.props.cards ? 
+                this.formatCard(this.state.popedCard)
+                : <Text>You can't answer quizes without cards</Text>}
+                {this.showScore()}
+            </View>
+        )
+    }
 }
 
 function mapStateToProps({ decks, cards }, { navigation }) {
